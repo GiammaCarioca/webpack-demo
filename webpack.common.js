303aa09b6002db9e9b63242b441429d06452a28c
Thiago Giammattey <giammattey@gmail.com>
2019-05-06 18:11:56 -0300

    minify HTML, CSS and JS in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,35 +1,28 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
 		main: './src/index.js',
 		vendor: './src/vendor.js'
 	},
-	plugins: [new HtmlWebpackPlugin({ template: './src/template.html' })],
 	module: {
 		rules: [
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			},
 			{
 				test: /\.html$/,
 				use: {
 					loader: 'html-loader'
-					}
+				}
 			},
 			{
-        test: /\.(svg|png|jpe?g|gif)$/,
-        use: 
-          {
-            loader: 'file-loader',
-            options: {
-							name: "[name].[hash].[ext]",
-							outputPath: "imgs"
-						},
-          },
-      },
+				test: /\.(svg|png|jpe?g|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash].[ext]',
+						outputPath: 'imgs'
+					}
+				}
+			}
 		]
 	}
 };
